15b062ce07f624da152783867bc871a03bbd2882
Stefan Fischer <stefischer@mail.com>
2024-04-16 08:41:48 -0400

    Add bidsController actions to indexes

---
+++ api/src/controllers/bids/index.ts
@@ -0,0 +1,5 @@
+export { default as acceptBid } from './acceptBid.action.js';
+export { default as create } from './create.action.js';
+export { default as destroy } from './destroy.action.js';
+export { default as getByCollectionId } from './getByCollectionId.action.js';
+export { default as update } from './update.action.js';
--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * as loginController from './auth/login/index.js';
 export * as signupController from './auth/signup/index.js';
 export * as collectionsController from './collections/index.js';
+export * as bidsController from './bids/index.js';
