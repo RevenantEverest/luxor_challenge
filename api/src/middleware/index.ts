08ee847d519971ca92cd5d0c421355ca8db8a296
Stefan Fischer <stefischer@mail.com>
2024-04-16 05:55:29 -0400

    Create id validation middleware

--- api/src/middleware/index.ts
+++ api/src/middleware/index.ts
@@ -1,3 +1,5 @@
 export { default as extractPaginationParams } from './extractPaginationParams.js';
 export { default as issueToken } from './issueToken.js';
 export { default as verifyToken } from './verifyToken.js';
+
+export * as validation from './validation/index.js';
