43a2160706d7b164dd9995dc4e5b9f8ca2feb3ca
Stefan Fischer <stefischer@mail.com>
2024-04-18 08:24:18 -0400

    Add BidStatus and BidCreate types

--- client/src/types/entities/Bid.ts
+++ client/src/types/entities/Bid.ts
@@ -1,10 +1,17 @@
+
+export type BidStatus = "pending" | "accepted" | "rejected";
+
 export interface Bid {
     id: number,
     price: number,
-    status: string,
+    status: BidStatus,
     created_at: Date,
     user: {
         id: string,
         email: string
     }
+};
+
+export interface BidCreate {
+    price: string
 };
