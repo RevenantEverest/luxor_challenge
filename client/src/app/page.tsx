a61223472a97b1b338fcef62e2f8d69064300f67
Stefan Fischer <stefischer@mail.com>
2024-04-18 04:35:39 -0400

    Display collections list in home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,59 @@
-import { Layout } from '@@components/Common';
+"use client"
 
+import type { Collection } from '@@types/entities/Collection';
 
+import { useState, useEffect, useCallback } from 'react';
+import { Card, Layout } from '@@components/Common';
+
+import CollectionListHeaders from '@@components/Collection/CollectionListHeaders';
+import CollectionCard from '@@components/Collection/CollectionCard';
+
+import * as api from '../api';
 
 function Home() {
+
+    const [loading, setLoading] = useState(false);
+    const [collections, setCollections] = useState<Collection[]>([]);
+
+    const fetchInitialCollections = useCallback(() => {
+        fetchCollections();
+    }, []);
+
+    useEffect(() => {
+        fetchInitialCollections();
+    }, [fetchInitialCollections]);
+
+    const fetchCollections = async (page = 1) => {
+        setLoading(true);
+        const [res, err] = await api.collectionServices.index({
+            page
+        });
+
+        if(err || !res) {
+            return console.error("Unable to fetch Collections");
+        }
+
+        setLoading(false);
+        setCollections(res.results);
+    };
+
+    const renderCollections = () => {
+        return collections.map((collection, index) => {
+            const key = `${collection.id}-${index}`;
+            const bgColor = index % 2 === 0 ? "bg-card" : "bg-card-light";
+            return(
+                <CollectionCard key={key} className={bgColor} collection={collection} />
+            );
+        });
+    };
+
     return (
-        <Layout>
-            <h1>Hello World</h1>
+        <Layout className="flex-col gap-10">
+            <h1 className="font-semibold text-4xl">Collections</h1>
+            <Card className="w-full min-h-[70vh]">
+                <CollectionListHeaders />
+                {loading ? "" : renderCollections()}
+            </Card>
         </Layout>  
     );
 };
